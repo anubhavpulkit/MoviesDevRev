5a979b4bb2daf4e5c7dcc45303df667876acb2ac
anubhavpulkit <anubhavssingh177@gmail.com>
2024-02-19 16:18:53 +0530

    Initial Commit

---
+++ MovieDevRev/MovieDevRevApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDevRevApp.swift
+//  MovieDevRev
+//
+//  Created by Anubhav Singh on 19/02/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieDevRevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
